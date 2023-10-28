9e958a7f90349d2cbc58d7d2104b3a1bb3eb45f0
debankanow@gmail.com <debankanow@gmail.com>
2023-10-28 16:38:34 +0530

    Initial Commit

---
+++ 5_Statement.py
@@ -0,0 +1,14 @@
+######################################################################################################################
+# Instructions that a Python interpreter can execute are called statements.
+# Example:  a = 1;  #single statement
+# Multi-line Statement
+# In python , end of a statement is marked by a newline character. But we can
+######################################################################################################################
+a = 1 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8
+# Or
+a = (1 + 2 + 3 +
+     4 + 5 + 6 +
+     7 + 8)
+print(a)
