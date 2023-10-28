151446183985690798db1558adfd43bc67295649
debankanow@gmail.com <debankanow@gmail.com>
2023-10-28 14:59:19 +0530

    Initial Commit

---
+++ 4_Indentation.py
@@ -0,0 +1,9 @@
+#####################################################################################################################
+# Python Indentation
+# Most of the programming languages like C,C++,Java use braces {} to define a block of code.
+# A code block (body of a function, loop etc) starts with indentation and ends with the first unindented line.
+# The amount of indentation and is preferred over tabs.
+# Generally four whitespaces are used for indentation and is preferred over tabs.
+#####################################################################################################################
+for i in range(5): #we will look at for loop.
+    print(i)
