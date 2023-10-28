48556e34346429bfe382b41bc1942cc191083cd7
debankanow@gmail.com <debankanow@gmail.com>
2023-10-28 14:08:13 +0530

    Initial Commit

---
+++ 2_Comment.py
@@ -0,0 +1,45 @@
+######################################################################################################################
+# Python Comments
+# In computer programming, comments are hints that we use to make our code more understandable.
+# Comments are lines that exist in computer programs and that are ignored by compilers and interpreters.
+#
+# Types of Comments in Python
+# In Python, there are two types of comments:
+#
+# single-line comment
+# multi-line comment
+#
+# Single-line Comment in Python
+# A single-line comment starts and ends in the same line. We use the # symbol to write a single-line comment. For example,
+######################################################################################################################
+
+# create a variable
+name = 'Eric Cartman'
+# print the value
+print(name)
+
+#
+# Here, we have created two single-line comments:
+#
+# # create a variable
+# # print the value
+# We can also use the single-line comment along with the code.
+#
+# name = 'Eric Cartman'  # name is a string
+
+######################################################################################################################
+# Another way of doing this is to use triple quotes, either ''' or """.
+# These triple quotes are generally used for multi-line strings. But if we do not assign it to any variable or function,
+# we can use it as a comment.
+# The interpreter ignores the string that is not assigned to any variable or function.
+# Let's see an example,
+######################################################################################################################
+
+''' This is also a
+perfect example of
+multi-line comments '''
+
+######################################################################################################################
+# Here, the multiline string isn't assigned to any variable, so it is ignored by the interpreter. Even though it is not
+# technically a multiline comment, it can be used as one.
+######################################################################################################################
